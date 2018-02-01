1e2540cb33eababa72130ff8e6a3afbe77230e08
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-01 13:53:37 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,7 @@ def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each do |attribute, value|
     if attribute== :favorite_ice_cream_flavor
       value.delete('strawberry')
+      binding.pry
     end
   end
 
