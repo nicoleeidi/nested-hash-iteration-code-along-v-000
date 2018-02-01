38f08fb59af704adc86b827e46cff3c43d7b1b7b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-01 13:53:18 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,16 +15,17 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  # contacts["Freddy Mercury"].each do |attribute, value|
-  #   if attribute== :favorite_ice_cream_flavor
-  #     value.delete('strawberry')
-  #   end
-  # end
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-       if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end
+  contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute== :favorite_ice_cream_flavor
+      value.delete('strawberry')
     end
   end
+  
+  # contacts.each do |person, contact_details_hash|
+  #   contact_details_hash.each do |attribute, data|
+  #      if attribute == :favorite_ice_cream_flavors
+  #         data.delete_if {|ice_cream| ice_cream == "strawberry"}
+  #     end
+  #   end
+  # end
 end
